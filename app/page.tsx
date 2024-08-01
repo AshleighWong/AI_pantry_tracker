336fc586d9313fac185cc5e7f04edfca2783b823
AshleighWong <ashleightwong@gmail.com>
2024-08-01 17:35:06 -0400

    import shad cn scroll area

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import Image from "next/image";
+'use client'
+import React, { useState, useEffect } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from './firebase';
+import { onSnapshot, query } from 'firebase/firestore';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
+
+
   return (
     <>
+
     </>
-  );
+  )
 }
